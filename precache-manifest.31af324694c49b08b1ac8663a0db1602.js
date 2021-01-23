1633493651c086ad8fc4fa2da4363206aa3e9638
viktor-gif <klim.victor.ua@gmail.com>
2021-01-23 15:55:04 +0200

    Updates

---
+++ precache-manifest.31af324694c49b08b1ac8663a0db1602.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52c8977fc0e83592f7fce51a117bc4b1",
+    "url": "/react-creativia/index.html"
+  },
+  {
+    "revision": "27ef78d1ca9817064cc4",
+    "url": "/react-creativia/static/css/main.1ab5f28e.chunk.css"
+  },
+  {
+    "revision": "a360086e780cd3d74ec8",
+    "url": "/react-creativia/static/js/2.51adf1e1.chunk.js"
+  },
+  {
+    "revision": "00e2b2cdc2475b76a408969f1af93df0",
+    "url": "/react-creativia/static/js/2.51adf1e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27ef78d1ca9817064cc4",
+    "url": "/react-creativia/static/js/main.b895df29.chunk.js"
+  },
+  {
+    "revision": "9d9dc95fa72f130d2c63",
+    "url": "/react-creativia/static/js/runtime-main.86a422d7.js"
+  },
+  {
+    "revision": "2765ed5f8f456b66ff4bfe7f1da07081",
+    "url": "/react-creativia/static/media/1.2765ed5f.jpeg"
+  },
+  {
+    "revision": "3e831cc788a856cefd01fde27a410ea7",
+    "url": "/react-creativia/static/media/1.3e831cc7.jpeg"
+  },
+  {
+    "revision": "968041cc564b94faf1d3ebf189d90e55",
+    "url": "/react-creativia/static/media/1.968041cc.jpeg"
+  },
+  {
+    "revision": "65e9a398811eb11759ce103a053d0d82",
+    "url": "/react-creativia/static/media/2.65e9a398.jpeg"
+  },
+  {
+    "revision": "7db9ab5d3f4814716c08046ce0dbe92c",
+    "url": "/react-creativia/static/media/3.7db9ab5d.jpeg"
+  },
+  {
+    "revision": "c987c9fe4b1fae6d6f23c07ed354ef86",
+    "url": "/react-creativia/static/media/4.c987c9fe.jpeg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-creativia/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8b8159e1f0741745700a61cf2ca71ee4",
+    "url": "/react-creativia/static/media/map.8b8159e1.jpeg"
+  },
+  {
+    "revision": "43f7925549b4f3336c19a70dd86987c2",
+    "url": "/react-creativia/static/media/titleImg.43f79255.jpeg"
+  }
+]);
